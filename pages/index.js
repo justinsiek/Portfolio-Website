2f14c05ff2cd9ff714881618e5cdac10b19af525
Justin Siek <justinsiek05@gmail.com>
2024-08-07 22:58:03 -0700

    pretending to be a dj

--- pages/index.js
+++ pages/index.js
@@ -34,11 +34,9 @@ const Index = () => {
         </div>
         <div className='flex-grow text-center font-integral'>
           <div className='inline-block'>
-            <Link href='/projects'>
-              <h1 className='hover:before:scale-x-100 hover:before:origin-left relative before:w-full before:h-1 before:origin-right
+            <h1 className='hover:before:scale-x-100 hover:before:origin-left relative before:w-full before:h-1 before:origin-right
               before:transition-transform before:duration-300 before:scale-x-0 before:bg-white before:absolute before:left-0 before:bottom-0'>
-                MIXES</h1>
-            </Link>
+              MIXES</h1>
           </div>
         </div>
         <div className='flex-grow text-center font-integral'>
